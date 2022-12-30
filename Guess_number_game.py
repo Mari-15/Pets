f1ecaa244a268c32ef1b594d73bc6134ea3c15b7
Voldemort <straymonax01@gmail.com>
2022-12-30 12:28:22 +0300

    add option 'new game' and counter

--- Guess_number_game.py
+++ Guess_number_game.py
@@ -12,6 +12,7 @@ number = random.randint(1, 100)
 print('Добро пожаловать в числовую угадайку!')
 print('Введите любое целое число от 1 до 100.')
 flag = False
+count = 0
 
 while not flag:
     number_user = input()
@@ -21,12 +22,30 @@ while not flag:
     number_user = int(number_user)
     if number_user < number:
         print('Ваше число меньше загаданного, попробуйте еще разок.')
+        count += 1
     elif number_user > number:
         print('Ваше число больше загаданного, попробуйте еще разок.')
+        count += 1
     elif number == number_user:
-        print('Поздравляем, вы угадали!')
-        flag = True
-        break
-    else:
-        print('К сожалению, не правильно. Попробуйте еще!')
+        count += 1
+        print('Вы угадали, поздравляем!')
+        print(f'Вы сделали {count} попыток.')
+        count = 0
+        print()
+        print('Хотите сыграть еще? Нажмите "д" для "да" и "н" для "нет".')
+        unswer = input()
+        flag_2 = False
+        while not flag_2:
+            if unswer.lower() == 'д':
+                number = random.randint(1, 100)
+                print('Играем еще раз!')
+                print('Мы задумали новое число. Дерзайте!')
+                flag_2 = True
+            elif unswer.lower() != 'н':
+                print('Вы ввели что-то непонятное. Попробуйте еще раз.')
+                unswer = input()
+            elif unswer.lower() == 'н':
+                print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+                flag = True
+                break
 
